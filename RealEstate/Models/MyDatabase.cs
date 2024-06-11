e56251af3c909ea7794e37d67b85de903b6151d7
ArtemUnreal71 <artemonych628@gmail.com>
2024-06-11 15:19:52 +0200

    About page template

--- RealEstate/Models/MyDatabase.cs
+++ RealEstate/Models/MyDatabase.cs
@@ -1,6 +1,15 @@
-﻿namespace RealEstate.Models
+﻿using Microsoft.EntityFrameworkCore;
+using Org.BouncyCastle.Asn1.Ocsp;
+
+namespace RealEstate.Models
 {
-    public class MyDatabase
+    public class MyDatabase : DbContext
     {
+        public DbSet<Account> Accounts { get; set; }     
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=myportfolioweb;user=root;password=;SslMode=none");
+        }
     }
 }
--- RealEstate/obj/Debug/net8.0/RealEstate.AssemblyInfo.cs
+++ RealEstate/obj/Debug/net8.0/RealEstate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7435bda049c1dd4db0ff07c52380ae3e6747c43d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
